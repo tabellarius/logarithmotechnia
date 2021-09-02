7612f812325d16a0fa6872764e2d06cf19af2304
dee-ru <di@dimon.ru>
2021-09-02 10:36:07 +0300

    Mutate()

--- dataframe/mutate.go
+++ dataframe/mutate.go
@@ -1,9 +1,21 @@
 package dataframe
 
-import "logarithmotechnia/vector"
+import (
+	"logarithmotechnia/vector"
+)
 
-func (df *Dataframe) Mutate(map[string]vector.Vector) *Dataframe {
-	panic("implement me")
+func (df *Dataframe) Mutate(columns map[string]vector.Vector, options ...vector.Option) *Dataframe {
+	/*
+		conf := vector.MergeOptions(options)
+
+		beforeColumnIndex := -1
+		afterColumnIndex := df.colNum-1
+
+		if conf.HasOption(vector.KeyOptionAfterColumn) {
+			afterColumn := conf.Value(vector.KeyOptionAfterColumn)
+		}
+	*/
+	return nil
 }
 
 func (df *Dataframe) Transmute(map[string]vector.Vector) *Dataframe {
