13ee4b99afdf3d98066724f5a376e83434b19669
dee-ru <di@dimon.ru>
2021-09-22 13:36:19 +0300

    Dataframe Rename()

---
+++ dataframe/rename.go
@@ -0,0 +1,27 @@
+package dataframe
+
+import (
+	"logarithmotechnia/vector"
+)
+
+type Rename struct {
+	from string
+	to   string
+}
+
+func (df *Dataframe) Rename(renames ...Rename) *Dataframe {
+	renamesMap := map[string]string{}
+	for _, rename := range renames {
+		renamesMap[rename.from] = rename.to
+	}
+
+	names := make([]string, df.colNum)
+	for i, columnName := range df.columnNames {
+		if newName, ok := renamesMap[columnName]; ok {
+			names[i] = newName
+		} else {
+			names[i] = columnName
+		}
+	}
+	return New(df.columns, vector.OptionColumnNames(names))
+}
